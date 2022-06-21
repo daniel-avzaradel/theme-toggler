4232d2f05012086ce334d0bdc0f5091ed8ec9dac
Daniel Avzaradel <daniela@cgen.com>
2022-06-21 15:11:27 +0300

    using useContext API to pass theme through a Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { useState, createContext, useContext } from 'react';
 import Form from './Form';
 
+export const ThemeContext = createContext(null);
+
 function App() {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => setTheme((cur) => (cur === 'light' ? 'dark' : 'light'));
   return (
-    <div>
-      <Form />
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div id={theme}>
+        <Form />
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
